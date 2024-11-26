1677f64b20063c448ee6c3078355fe69be505434
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-11-26 23:55:34 +0530

    .

---
+++ jasriti/src/components/sam.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const sam = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut placeat excepturi neque nulla ab odio, facere repudiandae, dicta sapiente, perferendis aliquam.</h1>
+    </div>
+  )
+}
+
+export default sam
